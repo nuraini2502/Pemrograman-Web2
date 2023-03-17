9f0adee8867a0979dc039ed523e69dbcd3c5fe64
nuraini2502 <rainii2502@gmail.com>
2023-03-17 19:18:23 +0700

    tugas pekan 3

--- pertemuan-3/proses_registrasi.php
+++ pertemuan-3/proses_registrasi.php
@@ -1,5 +1,6 @@
 <?php
-if (isset($_POST['submit']))
+
+if (isset($_POST['submit'])) {
     $nim = $_POST['nim'];
     $nama = $_POST['nama'];
     $gender = $_POST['gender'];
@@ -7,34 +8,52 @@ if (isset($_POST['submit']))
     $skills = $_POST['skill'];
     $domisili = $_POST['domisili'];
 
-function skor_skill($skills)
-{
-    $skill_list = [
-        'HTML' => 10,
-        'CSS' => 10,
-        'JavaScript' => 20,
-        'RWD Bootstrap' => 30,
-        'PHP' => 30,
-        'Python' => 30,
-        'Java' => 50,
-    ];
-    $result = 0;
+    function skor_skill($skills)
+    {
+        $skill_list = [
+            'HTML' => 10,
+            'CSS' => 10,
+            'JavaScript' => 20,
+            'RWD Bootstrap' => 30,
+            'PHP' => 30,
+            'Python' => 30,
+            'Java' => 50,
+        ];
+        $result = 0;
+        foreach ($skills as $skill) {
+            $result = $result + $skill_list[$skill];
+        }
+
+        return $result;
+    };
+
+    echo "NIM : $nim <br>";
+    echo "Nama : $nama <br>";
+    echo "Jenis Kelamin : $gender <br>";
+    echo "Programs Studi : $prodi <br>";
+    echo "Skill Programming : <br>";
+
     foreach ($skills as $skill) {
-        $result = $result + $skill_list[$skill];
+        echo "- $skill <br>";
     }
+    echo "Tempat Domisili : $domisili <br>";
+    echo "Skor Skill : " . skor_skill($skills);
+    echo "<br> Kategori Skill : ";
 
-    return $result;
-};
 
-
-echo "NIM : $nim <br>";
-echo "Nama : $nama <br>";
-echo "Jenis Kelamin : $gender <br>";
-echo "Programs Studi : $prodi <br>";
-echo "Skill Programming : <br>";
-foreach ($skills as $skill) {
-    echo "- $skill <br>";
+    if (skor_skill($skills) == 0) {
+        echo "Tidak Memadai";
+    } elseif (skor_skill($skills) >= 1 and skor_skill($skills) <= 40) {
+        echo "Kurang";
+    } elseif (skor_skill($skills) >= 41 and skor_skill($skills) <= 60) {
+        echo "Cukup";
+    } elseif (skor_skill($skills) >= 61 and skor_skill($skills) <= 100) {
+        echo "Baik";
+    } elseif (skor_skill($skills) >= 101 and skor_skill($skills) <= 150) {
+        echo "Sangat Baik";
+    } else {
+        echo "Tidak Ada Data Yang Cocok";
+    }
+} else {
+    echo "Error";
 }
-echo "Tempat Domisili : $domisili <br>";
-echo "Skor Skill : " . skor_skill($skills);
-echo "<br> Kategori Skill : ";
